817fc80a8cb172b1f0364d1808d57a5cdd4d8efb
James Whaley <juicy66173@gmail.com>
2019-11-07 13:18:31 +0000

    Add serialization support to Snowflake

--- src/model/snowflake.rs
+++ src/model/snowflake.rs
@@ -9,6 +9,10 @@
 use std::fmt::{self, Display};
 
 use chrono::{DateTime, TimeZone, Utc};
+use serde::{
+    de::{self, Deserialize, Deserializer},
+    ser::{Serialize, Serializer},
+};
 
 /// Discord epoch is the first second of 2015.
 const DISCORD_EPOCH: u64 = 1_420_070_400_000;
@@ -54,6 +58,12 @@ impl PartialEq<u64> for Snowflake {
     }
 }
 
+impl PartialEq<Snowflake> for u64 {
+    fn eq(&self, other: &Snowflake) -> bool {
+        *self == other.0
+    }
+}
+
 impl From<u64> for Snowflake {
     fn from(num: u64) -> Self {
         Snowflake(num)
@@ -63,5 +73,75 @@ impl From<u64> for Snowflake {
 impl From<Snowflake> for u64 {
     fn from(id: Snowflake) -> Self {
         id.0 as u64
+    }
+}
+
+impl Serialize for Snowflake {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_u64(self.0)
+    }
+}
+
+impl<'de> Deserialize<'de> for Snowflake {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct Visitor;
+
+        impl<'de> de::Visitor<'de> for Visitor {
+            type Value = u64;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+                formatter.write_str("a 64 bit snowflake id")
+            }
+
+            fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(v as u64)
+            }
+
+            fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(v)
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                v.parse::<u64>().map_err(de::Error::custom)
+            }
+        }
+
+        deserializer.deserialize_any(Visitor).map(Snowflake)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const ID: u64 = 80351110224678912;
+    const ID_STR: &'static str = "80351110224678912";
+
+    #[test]
+    fn test_serialize() {
+        let snowflake = Snowflake(ID);
+        let serialized = serde_json::to_string(&snowflake).unwrap();
+        assert_eq!(ID_STR, serialized);
+    }
+
+    #[test]
+    fn test_deserialize() {
+        let deserialized: Snowflake = serde_json::from_str(ID_STR).unwrap();
+        assert_eq!(Snowflake(ID), deserialized);
     }
 }
