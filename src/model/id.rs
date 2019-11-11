8ae30b2e03192089d6bc1d3d6495707228b4aa91
James Whaley <juicy66173@gmail.com>
2019-11-11 14:10:13 +0000

    Add missing derive Deserialize on ID types

--- src/model/id.rs
+++ src/model/id.rs
@@ -1,71 +1,71 @@
 //! Strongly-typed snowflake IDs.
 
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-use super::snowflake::Snowflake;
+use crate::model::snowflake::Snowflake;
 
 /// The ID of an [`Application`].
 ///
 /// [`Application`]: TODO
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
 pub struct ApplicationId(pub Snowflake);
 
 /// The ID of an [`Attachment`].
 ///
 /// [`Attachment`]: TODO
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
 pub struct AttachmentId(pub Snowflake);
 
 /// The ID of an [`AuditLogEntry`].
 ///
 /// [`AuditLogEntry`]: TODO
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
 pub struct AuditLogEntryId(pub Snowflake);
 
 /// The ID of a [`Channel`].
 ///
 /// [`Channel`]: TODO
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
 pub struct ChannelId(pub Snowflake);
 
 /// The ID of an [`Emoji`].
 ///
 /// [`Emoji`]: TODO
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
 pub struct EmojiId(pub Snowflake);
 
 /// The ID of a [`Guild`].
 ///
 /// [`Guild`]: TODO
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
 pub struct GuildId(pub Snowflake);
 
 /// The ID of an [`Integration`].
 ///
 /// [`Integration`]: TODO
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
 pub struct IntegrationId(pub Snowflake);
 
 /// The ID of a [`Message`].
 ///
 /// [`Message`]: TODO
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
 pub struct MessageId(pub Snowflake);
 
 /// The ID of a [`Role`].
 ///
 /// [`Role`]: TODO
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
 pub struct RoleId(pub Snowflake);
 
 /// The ID of a [`User`].
 ///
 /// [`User`]: TODO
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
 pub struct UserId(pub Snowflake);
 
 /// The ID of a [`Webhook`].
 ///
 /// [`Webhook`]: TODO
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Deserialize, Serialize)]
 pub struct WebhookId(pub Snowflake);
