2fa78c5110f1839c7a512f488a22906fe5797583
James Whaley <juicy66173@gmail.com>
2019-12-01 16:32:14 +0000

    Use thiserror to handle library Error

--- src/error.rs
+++ src/error.rs
@@ -1,27 +1,16 @@
-use std::{
-    error::Error as StdError,
-    fmt::{self, Display, Error as FormatError},
-    result::Result as StdResult,
-};
+use std::{fmt, result};
+
+use thiserror::Error;
 
 /// The common result type returned by library functions.
-pub type Result<T> = StdResult<T, Error>;
+pub type Result<T> = result::Result<T, Error>;
 
 /// The common error enum returned by library functions within a [`Result`].
 ///
 /// [`Result`]: type.Result.html
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
     /// An error while formatting a message.
-    Format(FormatError),
+    #[error("format error: {0}")]
+    Format(#[from] fmt::Error),
 }
-
-impl Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {
-            Error::Format(ref inner) => inner.fmt(f),
-        }
-    }
-}
-
-impl StdError for Error {}
