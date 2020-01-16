98b992bb1e4f5f04b0bb9c072852f65d49416b39
James Whaley <juicy66173@gmail.com>
2020-01-16 20:42:04 +0000

    Fix current_user example
    Fix current_user to not fail when run on CI.

--- examples/current_user.rs
+++ examples/current_user.rs
@@ -1,16 +1,22 @@
+use std::env;
+use std::fs;
+
 use strife::http::unstable::{Request, Route};
 use strife::model::user::ClientUser;
 use strife::{Error, Http};
-
-const DISCORD_TOKEN: &str = include_str!(".token");
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     // Initialize pretty logging.
     pretty_env_logger::init();
 
+    // Get token from `DISCORD_TOKEN` environment variable or `.token` file.
+    let token = env::var("DISCORD_TOKEN")
+        .or_else(|_| fs::read_to_string(".token"))
+        .expect("missing discord token");
+
     // Create an http client using our token.
-    let http = Http::new(DISCORD_TOKEN);
+    let http = Http::new(token);
 
     // Request information on the current user.
     let user: ClientUser = http.request(Request::new(Route::GetCurrentUser)).await?;
