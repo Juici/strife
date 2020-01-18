ad945701ad68710ebc0a548e3390ad53d8456b5d
James Whaley <juicy66173@gmail.com>
2020-01-18 21:14:02 +0000

    Update current_user example

--- examples/current_user.rs
+++ examples/current_user.rs
@@ -1,19 +1,17 @@
-use std::env;
-use std::fs;
+use std::{env, fs};
 
+use anyhow::Result;
 use strife::http::unstable::{Request, Route};
+use strife::http::Http;
 use strife::model::user::ClientUser;
-use strife::{Error, Http};
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<()> {
     // Initialize pretty logging.
     pretty_env_logger::init();
 
     // Get token from `DISCORD_TOKEN` environment variable or `.token` file.
-    let token = env::var("DISCORD_TOKEN")
-        .or_else(|_| fs::read_to_string(".token"))
-        .expect("missing discord token");
+    let token = env::var("DISCORD_TOKEN").or_else(|_| fs::read_to_string(".discord_token"))?;
 
     // Create an http client using our token.
     let http = Http::new(token);
